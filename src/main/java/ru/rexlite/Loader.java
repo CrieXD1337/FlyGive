530a226f1edd9e39a70202d04b1330ef758f6410
Ivan <sunblog0bg@gmail.com>
2025-04-14 23:43:46 +0300

    Create Loader.java

---
+++ src/main/java/ru/rexlite/Loader.java
@@ -0,0 +1,46 @@
+package ru.rexlite;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.Listener;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+import java.io.File;
+
+public class Loader extends PluginBase implements Listener {
+    public Loader() {
+    }
+
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        this.saveResource("config.yml");
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
+        Config cfg = new Config(new File(this.getDataFolder(), "config.yml"));
+        if (cmd.getName().toLowerCase().equals("flygive")) {
+            if (args.length < 2) {
+                sender.sendMessage(TextFormat.colorize(cfg.getString("use")));
+            } else if (this.getServer().getPlayer(args[0]) != null) {
+                if (args[1].equals("true")) {
+                    this.getServer().getPlayer(args[0]).setAllowFlight(true);
+                    sender.sendMessage(TextFormat.colorize(cfg.getString("true-fly").replace("{nick}", this.getServer().getPlayer(args[0]).getName())));
+                    if (cfg.getBoolean("is-use-messages-for-players")) {
+                        this.getServer().getPlayer(args[0]).sendMessage(TextFormat.colorize(cfg.getString("message-true")));
+                    }
+                } else {
+                    this.getServer().getPlayer(args[0]).setAllowFlight(false);
+                    sender.sendMessage(TextFormat.colorize(cfg.getString("false-fly").replace("{nick}", this.getServer().getPlayer(args[0]).getName())));
+                    if (cfg.getBoolean("is-use-messages-for-players")) {
+                        this.getServer().getPlayer(args[0]).sendMessage(TextFormat.colorize(cfg.getString("message-false")));
+                    }
+                }
+            } else {
+                sender.sendMessage(TextFormat.colorize(cfg.getString("no-found").replace("{nick}", args[0])));
+            }
+        }
+
+        return false;
+    }
+}
